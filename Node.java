fbb2b3e70f7a4f2f55cc64527e5a382303bf5ffe
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-08-13 12:39:34 +0200

    Changed Node to conform with NodeContent
    With NodeContent i use an interface to make sure that the stuff set as
    the nodes content, has specific methods. With this i can also remove the
    title of the node, since that is now the job of whatever implements the
    NodeContent interface.

--- Node.java
+++ Node.java
@@ -4,51 +4,43 @@ import java.util.List;
  * The class Node is used to represent a node in a network/graph. This node has
  * some content, a title, and its parents and children.
  */
-public class Node<T> {
+public class Node {
 
-    private String title;
-    private List<Node<T>> parents;
-    private List<Node<T>> children;
-    private T content;
+    private List<Node> parents;
+    private List<Node> children;
+    private NodeContent content;
 
-    public Node(String title, T content) {
+    public Node(NodeContent content) {
         this.content = content;
         this.parents = new ArrayList<>();
         this.children = new ArrayList<>();
     }
 
     /**
-     * Return the title of the node.
-     */
-    public String getTitle() {
-        return this.title;
-    }
-
-    /**
      * Return the content of the node.
      */
-    public T getContent() {
+    public NodeContent getContent() {
         return this.content;
     }
 
     /**
      * Add a node as a child to this node.
      */
-    public void addChild(Node<T> node) {
+    public void addChild(Node node) {
         this.children.add(node);
     }
 
     /**
      * Add a node as a parent to this node.
      */
-    public void addParent(Node<T> node) {
+    public void addParent(Node node) {
         this.parents.add(node);
     }
 
     /**
      * Return the children of this Node.
      */
-    public List<Node<T>> getChildren() {
+    public List<Node> getChildren() {
         return this.children;
     }
 
