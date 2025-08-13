22bf854b555f86022040ac0010c35e0afb48f055
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-08-13 12:37:31 +0200

    Created interface NodeContent

---
+++ NodeContent.java
@@ -0,0 +1,11 @@
+/**
+ * The interface NodeContent is used to force any content a node can have, to be
+ * able to return some sort of content, that can be used for other things.
+ */
+public interface NodeContent {
+
+    /**
+     * Get some content.
+     */
+    public String getContent();
+}
