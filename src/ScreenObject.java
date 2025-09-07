14bbddea543f78c0f0877b9aad1dd7f67202356e
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-09-07 20:58:01 +0200

    Added method comments

--- src/ScreenObject.java
+++ src/ScreenObject.java
@@ -5,7 +5,24 @@ import java.awt.Graphics;
  * can be placed on the screen.
  */
 public interface ScreenObject {
+    /**
+     * Draws the current ScreenObject on the given Graphics g.
+     */
     public void render(Graphics g);
+
+    /**
+     * Checks if the ScreenObject is under the given position.
+     */
     public boolean isUnder(int x, int y);
+
+    /**
+     * Returns whether this ScreenObject is in focus.
+     */
+    public boolean isFocused();
+
+    /**
+     * Sets the focus of this ScreenObject.
+     */
+    public void isFocused(boolean isFocused);
 }
 
