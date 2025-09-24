aea69a182f7be0fdb6082d6dd1d9d7fbccf59de3
Andreas Kapp Lindquist <andkaplin05@gmail.com>
2025-09-24 15:53:53 +0200

    Created MethodCaller interface

---
+++ src/MethodCaller.java
@@ -0,0 +1,15 @@
+import java.util.function.Consumer;
+
+/**
+ * MethodCaller
+ */
+public interface MethodCaller<T> {
+    /**
+     * Set the Method to be called.
+     */
+    public void setMethod(Consumer<T> method);
+    /**
+     * Call the method given to this class.
+     */
+    public void call();
+}
